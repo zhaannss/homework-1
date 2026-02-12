3ce8eb62556678e1b49f36664cb887fd5801f25c
Zhansaya <zhansaiabaikenova@gmail.com>
2026-02-13 00:55:14 +0500

    Добавлены персы но только шаблонно(без характристик и абилити)

---
+++ src/main/java/com/narxoz/rpg/character/Healer.java
@@ -0,0 +1,16 @@
+package com.narxoz.rpg.character;
+public class Healer extends BaseCh {
+    public Healer (String name, String race, int age, String gender) {
+        super(name, race, age, gender);
+        this.health = 0;
+        this.mana = 0;
+        this.strength = 0;
+        this.intelligence = 0;
+        this.agility = 0;
+    }
+    @Override
+    public void useSpecialAbility() {
+        System.out.println(name + " ");
+    }
+
+}
